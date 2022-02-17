573ac9a01934cdf3a559483b16748e34c51399a0
Abhishek Karna <48426023+abhishek2772@users.noreply.github.com>
2022-02-18 01:08:14 +0545

    Add files via upload

--- music/settings.py
+++ music/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^%#9nu_9_c3af6j@_)+=z^pbl&i_c7m+fm&zzlh$s8z9fl3ogc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abhishek2774.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -115,7 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
