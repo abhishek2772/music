15d286d4912adc474d726283dc6f9bf6eec7d19f
Abhishek Karna <48426023+abhishek2772@users.noreply.github.com>
2022-02-18 11:14:00 +0545

    Add files via upload

--- predictor/predict.py
+++ predictor/predict.py
@@ -4,9 +4,8 @@ def predict_gen(X):
     import keras
     import tensorflow as tf
     from django.conf import settings
-    print(os.listdir())
     
-    data = tf.keras.models.load_model(r'C:\Users\akarn\Desktop\mysite\login\models\my_model.h5')
+    data = tf.keras.models.load_model(r'predictor\models\my_model.h5')
     print(data)
     y = data.predict(X)
     apple = []
